a4e6b6a5abf8c50de823fe7fe5cf8a8d483ffbd4
Mondo <568197478@qq.com>
2021-05-12 14:45:52 +0800

    feat: 绿地

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+/**
+ * 整体手机屏幕宽屏查看
+ * 方位：
+ *     前
+ *
+ * 左  车位  右
+ *
+ *     后
+ */
 var canvas = document.getElementById('canvas'),
   WIDTH,
   HEIGHT;
@@ -62,25 +71,47 @@ DrawCarport.prototype.drawPort = function () {
   ctx.stroke();
   ctx.closePath();
 
-  // 小办公楼主体
+  // 右办公楼主体
   this.drawOffice(
     this.padding,
     this.portH - this.halveH,
     this.halveW - this.padding,
     this.halveH + this.padding
   );
-  // 大办公楼主体
+  // 前办公楼主体
   this.drawOffice(
     this.padding,
     this.padding,
     this.halveW - this.padding,
     this.portH - this.halveH - car_entry_w,
-    true
   );
+
+  // 右绿化带
+  this.drawGreenbelts(
+    this.padding + this.halveW - 4,
+    this.portH - this.halveH,
+    this.halveW*4 + 2,
+    this.halveH + this.padding
+  );
+
+  // 前绿化带
+  this.drawGreenbelts(
+    this.halveW - this.padding + 6.5,
+    this.padding + 2,
+    (this.halveW - this.padding) / 2,
+    this.portH - this.halveH - car_entry_w,
+  );
+
+  this.drawGreenLine(car_entry_w)
 };
 
 /**
  * 绘制办公楼
+ * @param {*} x 坐标x
+ * @param {*} y 坐标y
+ * @param {*} w 宽度
+ * @param {*} h 高度
+ * @param {*} hasText 是否绘制文字
  */
 DrawCarport.prototype.drawOffice = function (x, y, w, h, hasText) {
   ctx.beginPath();
@@ -95,18 +126,109 @@ DrawCarport.prototype.drawOffice = function (x, y, w, h, hasText) {
   ctx.stroke();
 
   if (hasText) {
-    ctx.translate(this.portW + this.padding, this.padding);
-    ctx.rotate(90 * Math.PI / 180)
+    ctx.save()
+    this.rotateContext(ctx, (this.portW + this.padding), this.padding, 90);
     ctx.fillStyle = '#333';
     ctx.font = '18px STheiti, SimHei';
-    ctx.fillText('办公楼', (x + w)*2.5, (y + h)/1.3);
+    ctx.fillText('办 公 楼', (x + w) * 7.5, (y + h) / 1.3);
+    ctx.restore()
   }
 };
 
 /**
+ * 旋转画布
+ * @param {*} ctx 
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} degree 
+ */
+DrawCarport.prototype.rotateContext = function(ctx, x, y, degree) {
+  ctx.translate(x, y);
+  ctx.rotate((degree * Math.PI) / 180);
+  ctx.translate(-x, -y);
+};
+
+/**
+ * 绘制绿化带
+ * @param {*} x 坐标x
+ * @param {*} y 坐标y
+ * @param {*} w 宽度
+ * @param {*} h 高度
+ * @param {*} hasText 是否绘制文字
+ */
+DrawCarport.prototype.drawGreenbelts = function (x, y, w, h) {
+  ctx.beginPath();
+  // 办公楼主体
+  ctx.rect(x, y, w, h);
+  // 填充颜色
+  ctx.fillStyle = '#85c226';
+  ctx.fill();
+};
+
+/**
+ * 绘制绿地线条
+ */
+DrawCarport.prototype.drawGreenLine = function(car_entry_w) {
+  // 10 等分
+  // 右边绿地
+  var green_w = this.halveW * 4;
+  var len = green_w / 6
+  for (let i = 0; i < 10; i++) {
+    ctx.beginPath()
+    var _w = this.padding + this.halveW - 4 + len * i;
+    ctx.moveTo(_w, this.portH - this.halveH)
+    var _h = this.portH - this.halveH + len * i;
+    ctx.lineTo(this.padding + this.halveW - 4, _h < 0 ? this.portH : _h)
+    ctx.strokeStyle = '#fff';
+    ctx.stroke()
+
+    ctx.beginPath()
+    var _w = this.padding + this.halveW - 4 + len * i + 10;
+    if (_w > this.portW) {
+      ctx.moveTo(_w, this.portH - this.halveH)
+    } else {
+      ctx.moveTo(_w, this.portH - this.halveH)
+    }
+    var _h = this.portH - this.halveH + len * i + 10;
+    ctx.lineTo(this.padding + this.halveW - 4, _h < 0 ? this.portH : _h)
+    ctx.strokeStyle = '#fff';
+    ctx.stroke()
+  }
+
+  var green_w_q = this.portH - this.halveH - car_entry_w;
+  var len_q = green_w_q / 5
+  for (let i = 0; i < 10; i++) {
+    ctx.beginPath()
+    var _w = this.halveW - this.padding + 6.5 + len_q * i;
+    var _h = this.padding + 2 + len_q * i;
+    if (_h > green_w_q) {
+      continue
+    }
+    ctx.moveTo(this.halveW - this.padding + 6.5, _h)
+    ctx.lineTo(_w, this.padding + 2)
+    ctx.strokeStyle = '#fff';
+    ctx.stroke()
+  }
+
+  for (let i = 0; i < 10; i++) {
+    ctx.beginPath()
+    var _w = this.halveW - this.padding + 6.5 + len_q * i + 10;
+    var _h = this.padding + 2 + len_q * i + 10;
+    // x + h
+    if (_h > green_w_q) {
+      continue;
+    }
+    ctx.moveTo(this.halveW - this.padding + 6.5, _h)
+    ctx.lineTo(_w, this.padding + 2)
+    ctx.strokeStyle = '#fff';
+    ctx.stroke()
+  }
+}
+
+/**
  * 绘制车位
  */
-DrawCarport.prototype.draw = function (x, y) {
+DrawCarport.prototype.drawCar = function (x, y) {
   ctx.beginPath();
   ctx.lineWidth = this.lineWidth;
   ctx.rect(x, y, CAR.width, CAR.height);
