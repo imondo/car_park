6c07afb9f195169e9c368c9e4c5a08a3d53c8ba9
Mondo <568197478@qq.com>
2021-05-12 10:22:47 +0800

    feat: 办公楼

--- main.js
+++ main.js
@@ -1,65 +1,113 @@
 var canvas = document.getElementById('canvas'),
-  ctx = canvas.getContext('2d'),
   WIDTH,
   HEIGHT;
+var ctx = canvas.getContext('2d');
 
 WIDTH = document.documentElement.clientWidth;
 HEIGHT = document.documentElement.clientHeight;
 
 var CAR = {
   width: 150,
-  height: 100
-}
+  height: 100,
+};
 
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 
 /**
  * 绘制类
- * @param {*} x 
- * @param {*} y 
+ * @param {*} x
+ * @param {*} y
  */
 function DrawCarport() {
-  this.bgColor = "#969593"; // 车位颜色
+  this.bgColor = '#969593'; // 车位颜色
   this.borderColor = '#ffffff'; // 车位border颜色
   this.lineWidth = 2; // 车位线宽度
+
+  this.padding = 5; // 停车场边缘宽度
+
+  this.portW = WIDTH - this.padding * 2;
+  this.portH = HEIGHT - this.padding * 2;
+  this.halveW = this.portW / 5;
+  this.halveH = this.portH / 4;
 }
 
 /**
  * 绘制停车场
  */
-DrawCarport.prototype.drawPort = function() {
-  var _padding = 5; // 停车场边缘宽度
-  var car_entry_w = 55 // 入口宽度
+DrawCarport.prototype.drawPort = function () {
+  var car_entry_w = 55; // 入口宽度
   // 停车场边缘 4个点
   // (5, 5) (WIDTH - 5, 5) (WIDTH - 5, HEIGHT - 5), (5, HEIGHT - 5)
-  // 停车场 长度 HEIGHT - 5  分为 4等分 HEIGHT - 5 / 4 
+  // 停车场 长度 HEIGHT - 5  分为 4等分 HEIGHT - 5 / 4
   // 停车场 宽度 WIDTH - 5 分为 5等分 WIDTH - 5 / 5
-  // 整个框架
-  var port_w = WIDTH - _padding;
-  var port_h = HEIGHT - _padding;
-  var halve_w = port_w / 5;
-  var halve_h = port_h / 4;
+  // 整个停车场边缘
+
   ctx.lineWidth = 4;
   ctx.beginPath();
-  ctx.moveTo(_padding, _padding)
-  ctx.lineTo(port_w, _padding)
-  ctx.lineTo(port_w, port_h)
-  ctx.lineTo(_padding, port_h)
-  ctx.lineTo(_padding, port_h - halve_h) // 入口拐点
-  ctx.lineTo(port_w - halve_w * 4, port_h - halve_h) // 入口拐点
-  ctx.lineTo(port_w - halve_w * 4, port_h - halve_h - car_entry_w) // 入口拐点
-  ctx.lineTo(_padding, port_h - halve_h - car_entry_w) // 入口拐点
-  ctx.lineTo(_padding, _padding) // 入口拐点
-  
-  ctx.strokeStyle = '#fffba0'
+  ctx.moveTo(this.padding, this.padding);
+  ctx.lineTo(this.portW + this.padding, this.padding);
+  ctx.lineTo(this.portW + this.padding, this.portH + this.padding);
+  ctx.lineTo(this.padding, this.portH + this.padding);
+  ctx.lineTo(this.padding, this.portH - this.halveH); // 入口拐点
+  ctx.lineTo(this.portW - this.halveW * 4, this.portH - this.halveH); // 入口拐点
+  ctx.lineTo(
+    this.portW - this.halveW * 4,
+    this.portH - this.halveH - car_entry_w
+  ); // 入口拐点
+  ctx.lineTo(this.padding, this.portH - this.halveH - car_entry_w); // 入口拐点
+  ctx.lineTo(this.padding, this.padding); // 入口拐点
+
+  ctx.strokeStyle = '#fffba0';
   ctx.stroke();
-}
+  ctx.closePath();
+
+  // 小办公楼主体
+  this.drawOffice(
+    this.padding,
+    this.portH - this.halveH,
+    this.halveW - this.padding,
+    this.halveH + this.padding
+  );
+  // 大办公楼主体
+  this.drawOffice(
+    this.padding,
+    this.padding,
+    this.halveW - this.padding,
+    this.portH - this.halveH - car_entry_w,
+    true
+  );
+};
+
+/**
+ * 绘制办公楼
+ */
+DrawCarport.prototype.drawOffice = function (x, y, w, h, hasText) {
+  ctx.beginPath();
+  // 办公楼主体
+  ctx.lineWidth = 1;
+  ctx.rect(x, y, w, h);
+  // 填充颜色
+  ctx.fillStyle = '#b6ddca';
+  ctx.fill();
+
+  ctx.strokeStyle = '#515435';
+  ctx.stroke();
+
+  if (hasText) {
+    ctx.translate(this.portW + this.padding, this.padding);
+    ctx.rotate(90 * Math.PI / 180)
+    ctx.fillStyle = '#333';
+    ctx.font = '18px STheiti, SimHei';
+    ctx.fillText('办公楼', (x + w)*2.5, (y + h)/1.3);
+  }
+};
 
 /**
  * 绘制车位
  */
-DrawCarport.prototype.draw = function(x, y) {
+DrawCarport.prototype.draw = function (x, y) {
+  ctx.beginPath();
   ctx.lineWidth = this.lineWidth;
   ctx.rect(x, y, CAR.width, CAR.height);
 
@@ -70,8 +118,8 @@ DrawCarport.prototype.draw = function(x, y) {
   // 描边
   ctx.strokeStyle = this.borderColor;
   ctx.stroke();
-}
+};
 
-var drawCarport = new DrawCarport()
+var drawCarport = new DrawCarport();
 
 drawCarport.drawPort();
